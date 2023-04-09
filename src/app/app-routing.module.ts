da49f0da9b047f3397aedce08a74567db872aac3
azizazouni <azizazouni5@gmail.com>
2023-04-09 02:12:13 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'accueil', component:HomeComponent},
+{path:'categorie', component:CategorieComponent},
+{path:'detail', component:DetailComponent},
+{path:'panier', component:CartComponent},
+
+{path:'', redirectTo:'accueil', pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
